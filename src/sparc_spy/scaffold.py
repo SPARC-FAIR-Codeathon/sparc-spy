b29a384f254308490521b87854e56fffe1eac518
Savindi Wijenayaka <savindi.narmada@gmail.com>
2024-08-13 00:00:46 +1200

    Remove redandunt information from method signature

--- src/sparc_spy/scaffold.py
+++ src/sparc_spy/scaffold.py
@@ -94,14 +94,11 @@ class Scaffold(object):
     def __read_jsons(self, dir: str):
         return [os.path.join(dir, file) for file in os.listdir(dir) if file.endswith(".json")]
 
-    def build_scaffold(self, derivative_dir: str) -> pv.PolyData:
+    def build_scaffold(self, derivative_dir: str):
         """Create Scaffold from existing meshes and geometry.
 
         Args:
             derivative_dir (str): Derivative directory containing JSON files.
-
-        Returns:
-            pv.PolyData: Scaffold containing Geometry and meshes
         """
         for surface in self.metadata["Surfaces"]:
             label = surface["label"].replace(" ", "_") if surface["label"] != "" else "unnamed"
