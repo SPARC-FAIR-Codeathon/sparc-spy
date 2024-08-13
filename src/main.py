06bf6767ff0a2b3d53f2cbdce68801ceb75ed20f
Savindi Wijenayaka <savindi.narmada@gmail.com>
2024-08-13 15:27:37 +1200

    Add colour data support

--- src/main.py
+++ src/main.py
@@ -1,13 +1,11 @@
 from sparc_spy import Scaffold
 
 scaffold = Scaffold("rat_gut", derivative_dir="example_data/files/derivative")
-
 scaffold.get_mesh_details()
-
 scaffold.plot()
-
 scaffold.update_mesh_label(original_name="unnamed", new_name="whole_stomach")
-
 scaffold.plot()
-
 scaffold.export("output.stl")
+
+scaffold = Scaffold("rat_intervension", derivative_dir="example_data/derivative/sub-112")
+scaffold.plot()
